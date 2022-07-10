65331cb48d5624ffd9b64038e59b02f719cf3538
bagashiz <bagashiz@proton.me>
2022-07-10 16:52:16 +0700

    added comments to 3sum.go

--- Medium/3sum.go
+++ Medium/3sum.go
@@ -60,8 +60,7 @@ func threeSum(nums []int) [][]int {
 
 func main() {
 	// test cases
-	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
-	fmt.Println(threeSum([]int{0, 1, 1}))
-	fmt.Println(threeSum([]int{0, 0, 0}))
-
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4})) // [[-1 -1 2], [-1 0 1]]
+	fmt.Println(threeSum([]int{0, 1, 1}))             // []
+	fmt.Println(threeSum([]int{0, 0, 0}))             // [[0 0 0]]
 }
